743eff828e65be941fe617aadfe690d3d695aa19
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-06-12 19:24:07 -0300

    Criando a opção de selecionar países

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,15 +1,24 @@
 console.log('teste')
 
-fetch('https://api.covid19api.com/summary').then( (res) => {
-    res.json().then( (data) => {
+getData('BR')
 
-        let country = data.Countries.filter(dt => dt.CountryCode == 'BR');
+$( "#select-country" ).change(function() {
+    getData($( "#select-country" ).val())
+})
 
-        $("#newConfirmed").text(country[0].NewConfirmed);
-        $("#totalConfirmed").text(country[0].TotalConfirmed);
-        $("#newDeaths").text(country[0].NewDeaths);
-        $("#totalDeaths").text(country[0].TotalDeaths);
-        $("#newRecovered").text(country[0].NewRecovered);
-        $("#totalRecovered").text(country[0].TotalRecovered);
-    })
-} )
+function getData (countryCode) {
+    fetch('https://api.covid19api.com/summary').then( (res) => {
+        res.json().then( (data) => {
+
+            let country = data.Countries.filter(dt => dt.CountryCode == countryCode);
+
+            $("#newConfirmed").text(country[0].NewConfirmed);
+            $("#totalConfirmed").text(country[0].TotalConfirmed);
+            $("#newDeaths").text(country[0].NewDeaths);
+            $("#totalDeaths").text(country[0].TotalDeaths);
+            $("#newRecovered").text(country[0].NewRecovered);
+            $("#totalRecovered").text(country[0].TotalRecovered);
+        })
+    } )
+}
+
