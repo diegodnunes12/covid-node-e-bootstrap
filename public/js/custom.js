f3da6e66f84d87c700ac56f5fab70936f64e2a2b
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-06-16 17:12:46 -0300

    Reorganização do layout

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,8 +2,11 @@ console.log('teste')
 
 getData('BR')
 
-$( "#select-country" ).change(function() {
-    getData($( "#select-country" ).val())
+$('#select-country').change(function() {
+    let country = $('#select-country').val()
+    
+    $('#flag').attr('src', `/images/${country}.png`)
+    getData(country)
 })
 
 function getData (countryCode) {
@@ -12,12 +15,12 @@ function getData (countryCode) {
 
             let country = data.Countries.filter(dt => dt.CountryCode == countryCode);
 
-            $("#newConfirmed").text(country[0].NewConfirmed.toLocaleString('pt-br'));
-            $("#totalConfirmed").text(country[0].TotalConfirmed.toLocaleString('pt-br'));
-            $("#newDeaths").text(country[0].NewDeaths.toLocaleString('pt-br'));
-            $("#totalDeaths").text(country[0].TotalDeaths.toLocaleString('pt-br'));
-            $("#newRecovered").text(country[0].NewRecovered.toLocaleString('pt-br'));
-            $("#totalRecovered").text(country[0].TotalRecovered.toLocaleString('pt-br'));
+            $('#newConfirmed').text(country[0].NewConfirmed.toLocaleString('pt-br'));
+            $('#totalConfirmed').text(country[0].TotalConfirmed.toLocaleString('pt-br'));
+            $('#newDeaths').text(country[0].NewDeaths.toLocaleString('pt-br'));
+            $('#totalDeaths').text(country[0].TotalDeaths.toLocaleString('pt-br'));
+            $('#newRecovered').text(country[0].NewRecovered.toLocaleString('pt-br'));
+            $('#totalRecovered').text(country[0].TotalRecovered.toLocaleString('pt-br'));
         })
     } )
 }
