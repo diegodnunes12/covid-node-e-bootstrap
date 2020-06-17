4f745f4994fbfb0fd15fb015ce8fb47eb2883017
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-06-17 18:50:43 -0300

    Inserindo a data de atualização dos paises

--- public/js/custom.js
+++ public/js/custom.js
@@ -13,15 +13,33 @@ function getData (countryCode) {
     fetch('https://api.covid19api.com/summary').then( (res) => {
         res.json().then( (data) => {
 
-            let country = data.Countries.filter(dt => dt.CountryCode == countryCode);
+            let country = data.Countries.filter(dt => dt.CountryCode == countryCode)
+            let countryDate = new Date(country[0].Date)
 
-            $('#newConfirmed').text(country[0].NewConfirmed.toLocaleString('pt-br'));
-            $('#totalConfirmed').text(country[0].TotalConfirmed.toLocaleString('pt-br'));
-            $('#newDeaths').text(country[0].NewDeaths.toLocaleString('pt-br'));
-            $('#totalDeaths').text(country[0].TotalDeaths.toLocaleString('pt-br'));
-            $('#newRecovered').text(country[0].NewRecovered.toLocaleString('pt-br'));
-            $('#totalRecovered').text(country[0].TotalRecovered.toLocaleString('pt-br'));
+            $('#updatedDate').text(formatDate(countryDate))
+
+            $('#newConfirmed').text(country[0].NewConfirmed.toLocaleString('pt-br'))
+            $('#totalConfirmed').text(country[0].TotalConfirmed.toLocaleString('pt-br'))
+            $('#newDeaths').text(country[0].NewDeaths.toLocaleString('pt-br'))
+            $('#totalDeaths').text(country[0].TotalDeaths.toLocaleString('pt-br'))
+            $('#newRecovered').text(country[0].NewRecovered.toLocaleString('pt-br'))
+            $('#totalRecovered').text(country[0].TotalRecovered.toLocaleString('pt-br'))
         })
     } )
+}
+
+function formatDate(date){
+    let day = twoDigitsFormat(date.getDate())
+    let month = twoDigitsFormat(date.getMonth() + 1)
+    let year = date.getFullYear()
+    let hour = twoDigitsFormat(date.getHours())
+    let minute = twoDigitsFormat(date.getMinutes())
+    let second = twoDigitsFormat(date.getSeconds())
+
+    return (`${day}/${month}/${year} ${hour}:${minute}:${second}`)
+}
+
+function twoDigitsFormat(date){
+    return ("0" + (date)).slice(-2)
 }
 
